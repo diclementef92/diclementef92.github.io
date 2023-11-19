e4cb0c0538a2abb63b52f2d28058c0b3d0e76c75
diclementef92 <diclementef92@gmail.com>
2023-11-19 17:37:43 +0100

    new sidebar offcanvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
-$("#sidebarCollapse").on("click", function () {
-  $("#sidebar").toggleClass("active");
-});
+window.onload = () => {
+  document
+    .getElementsByClassName("profile-img")[0]
+    .addEventListener("click", function () {
+      var img = this;
+
+      setTimeout(function () {
+        img.src = "../assets/img/profilo1.jpg";
+        img.style.opacity = 1;
+      }, 500);
+    });
+};
