bd9e3f8b3e39f053aafe4abd688dbdfbc25ad12e
diclementef92 <diclementef92@gmail.com>
2023-12-05 15:08:11 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
 window.onload = () => {
   var counter = 0;
-  const imgs = ["../assets/img/profilo2.jpg", "../assets/img/profilo3.jpg"];
+  const imgs = [
+    "../assets/img/profilo2.jpg",
+    "../assets/img/profilo3.jpg",
+    "../assets/img/profilo1.jpg",
+  ];
   document
     .getElementsByClassName("profile-img")[0]
     .addEventListener("click", function () {
       var img = this;
       if (counter < imgs.length) {
+        img.src = imgs[counter];
+        counter++;
+      } else {
+        counter = 0;
         img.src = imgs[counter];
         counter++;
       }
