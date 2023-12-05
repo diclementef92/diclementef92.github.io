7d646c1b8265b0f9244e4b9dcecfcee5b3aec29f
diclementef92 <diclementef92@gmail.com>
2023-12-05 15:01:10 +0100

    change image in chisono

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 window.onload = () => {
+  var counter = 0;
+  const imgs = ["../assets/img/profilo2.jpg", "../assets/img/profilo3.jpg"];
   document
     .getElementsByClassName("profile-img")[0]
     .addEventListener("click", function () {
       var img = this;
-
-      setTimeout(function () {
-        img.src = "../assets/img/profilo1.jpg";
-        img.style.opacity = 1;
-      }, 500);
+      if (counter < imgs.length) {
+        img.src = imgs[counter];
+        counter++;
+      }
     });
 };
